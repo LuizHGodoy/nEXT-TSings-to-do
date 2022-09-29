8683904b23b5af29eb20ac3a223c84dfdbffa69d
LuizGodoy <luizhenriquegodoydeoliveira03@gmail.com>
2022-09-29 07:28:37 -0400

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import styles from './styles.module.scss'
+
 export default function Home() {
-  return <div></div>
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>This is my to do YO</h1>
+    </div>
+  )
 }
